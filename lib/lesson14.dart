b07509bf93152d52c2490ed507e2b1bbf2e2eb3c
tdthien106 <tdthien.work@gmail.com>
2024-03-06 10:33:48 +0700

    Weather app

--- lib/lesson14.dart
+++ lib/lesson14.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class lesson14 extends StatelessWidget {
+  const lesson14({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
